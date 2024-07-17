b9f223b01a2570f123d1a1f346e0b60f58f7d35e
toskasogody <toska.shala@sogody.com>
2024-07-17 16:10:24 +0200

    Fixed the hamburger of the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,36 +1,33 @@
-// src/components/Navbar.js
+// src/components/navbar.js
 import React from 'react';
 import { Link } from 'gatsby';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../images/logo.png';
 
-
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-        <img src={logo} alt="Logo" className="navbar-logo" />
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/products">Products</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart">Cart</Link>
-            </li>
-          </ul>
-        </div>
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">
+        <img src={logo} alt="Logo" className="navbar-logo" style={{ height: '40px' }} />
+        
+      </Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/products">Products</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/cart">Cart</Link>
+          </li>
+        </ul>
       </div>
-    </nav>
-  );
-};
+    </div>
+  </nav>
+);
 
 export default Navbar;
